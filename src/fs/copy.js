1aa246bb44fb02675b07271e5be9990ba18d88c8
Romanenkov Dmitriy <215rd@mail.ru>
2024-05-19 21:58:32 +0300

    feat: complete "copy" task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import fs from "fs/promises";
+
+const ERROR_MESSAGE = "FS operation failed";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.cp("./src/fs/files/", "./src/fs/files_copy/", {
+      recursive: true,
+      errorOnExist: true,
+      force: false,
+    });
+  } catch (e) {
+    if (e.code === "ENOENT" || e.code === "ERR_FS_CP_EEXIST") {
+      console.log(ERROR_MESSAGE);
+    } else {
+      throw e;
+    }
+  }
 };
 
 await copy();
