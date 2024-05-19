b083b651d574ba93914b6f3a6ff6ad8f7739736d
Romanenkov Dmitriy <215rd@mail.ru>
2024-05-19 20:46:07 +0300

    feat: complete "create" task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from "fs/promises";
+
+const TEXT = "I am fresh and young";
+const ERROR_MESSAGE = "FS operation failed";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.writeFile("./src/fs/files/fresh.txt", TEXT, { flag: "wx" });
+  } catch (e) {
+    if (e.code === "EEXIST") {
+      console.log(ERROR_MESSAGE);
+    } else {
+      throw e;
+    }
+  }
 };
 
 await create();
