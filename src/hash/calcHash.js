a554b350cdbccba0fa8c544f1fbdfb60815b3c72
Romanenkov Dmitriy <215rd@mail.ru>
2024-05-26 18:40:14 +0300

    feat: solve hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import crypto from "crypto";
+import { createReadStream } from "fs";
+import path from "path";
+import { throwFsError } from "../helpers/throwFsError.js";
+
+const filePath = path.join(
+  import.meta.dirname,
+  "files",
+  "fileToCalculateHashFor.txt"
+);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = crypto.createHash("sha256");
+  hash.setEncoding("hex");
+
+  const readStream = createReadStream(filePath);
+
+  readStream.on("end", () => {
+    hash.end();
+    console.log(hash.read());
+  });
+  readStream.on("error", (e) => throwFsError(e));
+
+  readStream.pipe(hash);
 };
 
 await calculateHash();
