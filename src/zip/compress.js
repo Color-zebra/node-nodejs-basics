8c62ebfe161917d4090a547908d4ec86c42b8901
Romanenkov Dmitriy <215rd@mail.ru>
2024-05-27 22:44:26 +0300

    feat: solve zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import stream from "stream";
+import path from "path";
+import zlib from "zlib";
+import { throwFsError } from "../helpers/throwFsError.js";
+
+const inputPath = path.join(import.meta.dirname, "files", "fileToCompress.txt");
+const outputPath = path.join(import.meta.dirname, "files", "archive.gz");
+
 const compress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(inputPath);
+  const writeStream = fs.createWriteStream(outputPath);
+  const archiveStream = zlib.createGzip();
+
+  stream.pipeline(readStream, archiveStream, writeStream, (e) => {
+    if (e) {
+      fs.unlink(outputPath, () => throwFsError(e));
+    }
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+import stream from "stream";
+import path from "path";
+import zlib from "zlib";
+import { throwFsError } from "../helpers/throwFsError.js";
+
+const inputPath = path.join(import.meta.dirname, "files", "archive.gz");
+const outputPath = path.join(
+  import.meta.dirname,
+  "files",
+  "fileToCompressTest.txt"
+);
 const decompress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(inputPath);
+  const writeStream = fs.createWriteStream(outputPath);
+  const decompressStream = zlib.createUnzip();
+
+  stream.pipeline(readStream, decompressStream, writeStream, async (e) => {
+    if (e) {
+      fs.unlink(outputPath, () => throwFsError(e));
+    }
+  });
 };
 
 await decompress();
