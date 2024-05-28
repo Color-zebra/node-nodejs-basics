19d3c904af83bcdb85aac6b3afb432cc147c501a
Romanenkov Dmitriy <215rd@mail.ru>
2024-05-28 23:22:59 +0300

    feat: solve cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import childProcess from "child_process";
+import path from "path";
+
+const childProcessFilePath = path.join(
+  import.meta.dirname,
+  "files",
+  "script.js"
+);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = childProcess.spawn(`node`, [childProcessFilePath, ...args]);
+  process.stdin.on("data", (data) => child.stdin.write(data));
+  child.stdout.on("data", (data) => process.stdout.write(data.toString()));
+  child.on("exit", () => process.exit());
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+
+spawnChildProcess(["firstArg", "secondArg", "thirdArg"]);
